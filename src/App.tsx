055688ea3853676dd9446f77050c9639938d8337
KRISH <addresskrish@gmail.com>
2025-11-05 17:34:23 +0530

    serviceComponet added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Services from "./components/Services"
 
 const App = () => {
   return (
     <main className="overflow-x-hidden antialiased text-neutral-800">
       <Navbar />
       <Hero />
+      <Services />
     </main>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,8 @@ const Navbar = () => {
         setIsOpen(!isOpen)
     }
     return (
-        <nav className="border-b-2">
-            <div className="max-w-7xl mx-auto flex justify-between items-center py-8">
+        <nav className="max-w-7xl mx-auto border-b-2">
+            <div className="flex justify-between items-center py-8">
                 <div className="pl-2">
                     <a href="#">
                         <img src={logo} width={150} height={15} alt="VastuSpaze"/>
